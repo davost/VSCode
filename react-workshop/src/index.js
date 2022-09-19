50ab42283c15f18d8bceb876e7e14fb7dd06c640
davost <bildermejl@gmail.com>
2022-09-19 16:10:45 +0200

    index.js, index.html, install and import bootstrap

--- react-workshop/src/index.js
+++ react-workshop/src/index.js
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<h2>Hello React!</h2>, document.getElementById('root') );
+ReactDOM.render(<h2>Hello React!!</h2>, document.getElementById('root') );
