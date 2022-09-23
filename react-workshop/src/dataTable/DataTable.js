c8b4724f316561119b2c790116797e18e8a7054e
davost <bildermejl@gmail.com>
2022-09-23 22:35:43 +0200

    All I can do. 2 errors left.

--- react-workshop/src/dataTable/DataTable.js
+++ react-workshop/src/dataTable/DataTable.js
@@ -1,21 +1,65 @@
 import React, { useEffect , useState } from 'react';
-import Table from './Table';
 
 const DataTable = () => {
-    const studentList = { id:1, firstName: "Test", lastName: "Test", age: 0, birthdate: new Date("0000-00-00") };
-    const [dataTable, setDataTable] = useState(studentList);
-
-    useEffect( () => {
-        console.log('useEffect function hass been executed');
-       setDataTable([
-        { id:1, firstName: "Sven", lastName: "Svensson", age: 20, birthdate: new Date("2002-08-21") },
-        { id:2, firstName: "Tove", lastName: "Tuvesson", age: 25, birthdate: new Date("1997-07-24") },
-        { id:3, firstName: "Johan", lastName: "Larsson", age: 30, birthdate: new Date("1992-05-15") }] , [] );
+    const initialState = [
+        { id:0, firstName: "Test", lastName: "Test", age: 0, birthdate: new Date("0000-00-00"), country: "Sweden", city: "Stockholm" },
+        { id:1, firstName: "Sven", lastName: "Svensson", age: 20, birthdate: new Date("2002-08-21"), country: "Sweden", city: "Kalmar" },
+    ];
+    const [studentList, setStudentList] = useState(initialState);
+    
+    // create header component that returns the header information of table
+    const TableHeader = () => {
+        return ( <thead>
+            <tr>
+                <th>Id</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Age</th>
+                <th>Birthdate</th>
+                <th>Country</th>
+                <th>City</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        );
+    };
+    
+    const TableRow = (props) => {
+    const [tableRows, setTableRows] = useState([]);
+        useEffect(() => {
+            setTableRows(props.list);
+        }, []);
+    
+        return ( 
+            <tbody>
+            {tableRows.map((student) => {
+                const row = (
+                    <tr key={student.id}>
+                        <td>{student.id}</td>
+                        <td>{student.firstName}</td>
+                        <td>{student.lastName}</td>
+                        <td>{student.age}</td>
+                        //<td>
+                        //<TableAction student={student} />
+                        //</td>
+                    </tr>
+                );
+                return row;
+            })}
+           </tbody>
+           );
+        };
+    
+    
     return (
         <div>
-            <b>DataTable info: {dataTable.firstName} {dataTable.lastName}</b>
+            <h1>List of students</h1>
+            <table>
+                <TableHeader />
+                <TableRow />
+            </table>
         </div>
     );
-})};
+};
 
 export default DataTable;
